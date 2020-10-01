9154fd3e2181cdec6482af80bdc4a0fab6c0d586
Kaden Thomas <thomas.kaden4@outlook.com>
2020-10-01 00:47:02 -0700

    Add cruise-control process and process runner

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ![Logo](/public/favicon.ico)
 
 # PIDSim
+
+Built as a teaching exercise for the Instrumentation and Control Systems program at Bellingham Technical College.
--- src/pid.ts
+++ src/pid.ts
@@ -17,30 +17,53 @@ export interface PID {
   derivative: Derivative;
 }
 
-export interface TimeStep {
+export interface PIDController {
   (error: number, delta: number): number;
 }
 
-export function pidStep(pid: PID): TimeStep {
+export function pidStep(pid: PID): PIDController {
   let integral = 0;
   let previousError = 0;
   return (error, delta) => {
     previousError = error;
+    integral += pid.integral.coefficient * error * delta;
     const proportion = pid.proportion.proportionality * error + pid.proportion.bias;
     const derivative = pid.derivative.coefficient * ((error - previousError) / delta);
-    integral += pid.integral.coefficient * error * delta;
     return proportion + integral + derivative;
   };
 }
 
-// A process is an interaction between the manipulatable variable,
+// A process is an interaction between the manipulated variable,
 // over a period of time, producing a present value
 export interface Process {
   (m: number, delta: number): number;
 }
 
-export function cruiseControl(speed: number): Process {
+export function cruiseControl(startingSpeed: number, jerk: number): Process {
+  let currentSpeed = startingSpeed;
   return (pedal: number, delta: number) => {
-    return 0;
+    const acceleration = pedal * jerk;
+    const speedGain = acceleration * delta;
+    currentSpeed += speedGain;
+    return currentSpeed;
   };
+}
+
+export function runProcessWithPIDControl(
+  process: Process,
+  pid: PID,
+  initialOutput: number,
+  setPoint: number,
+  steps: number,
+  delta: number,
+  onStep: (pv: number) => void
+) {
+  let pv = process(initialOutput, 0);
+  let step = pidStep(pid);
+  let output = initialOutput;
+  for (let i = 0; i < steps; ++i) {
+    output = step(setPoint - pv, delta);
+    pv = process(output, delta);
+    onStep(pv);
+  }
 }
